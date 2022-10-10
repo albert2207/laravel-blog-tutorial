864ab6e58c5e7539ddba7da64507803850768728
netoplaydev <albert@bitandpay.com>
2022-10-10 16:02:48 +0300

    add user post relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,6 +23,13 @@ class Post extends Model
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category() {
         return $this->belongsTo(Category::class);
     }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,7 @@
 
     <article>
 <h1>{{ $post->title }}</h1>
+        By <a href="/users/{{$post->user->slug}}">{{$post->user->name}}</a> Category: <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
     <p>
         {!! $post->body !!}
     </p>
